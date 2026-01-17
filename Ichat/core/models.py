f5f453774eaa7730604b5e1b16d5a369740ac089
ashenafi <ashenafiadola05@gmail.com>
2026-01-17 11:25:24 +0300

    seted up models

--- Ichat/core/models.py
+++ Ichat/core/models.py
@@ -1,3 +1,28 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Group(models.Model):
+    owner = models.ForeignKey(User, on_delete=models.CASCADE)
+    name = models.CharField(max_length=100)
+    description = models.CharField(max_length=300)
+    members = models.ManyToManyField(User)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+
+    def __str__(self):
+        return self.name
+
+class Message(models.Model):
+    owner = models.ForeignKey(User, on_delete=models.CASCADE)
+    group = models.ForeignKey(Group, on_delete=models.CASCADE)
+
+    body = models.TextField()
+
+
+
+    def __str__(self):
+        return self.body[0:30]
+    
+class Channal(models.Model):
+    pass
