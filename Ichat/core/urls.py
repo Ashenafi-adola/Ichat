599878b2ca224e9c4b019edcfd8199c51792c401
ashenafi <ashenafiadola05@gmail.com>
2026-01-18 07:35:24 +0300

    added bootstrap to group page

--- Ichat/core/urls.py
+++ Ichat/core/urls.py
@@ -5,7 +5,7 @@ urlpatterns = [
     path('sign-up/', views.sign_up, name="sign-up"),
     path('sign-in/', views.log_in, name='log-in'),
     path('logout/', views.log_out, name='logout'),
-    path('home/', views.home, name='home'),
+    path('', views.home, name='home'),
     path('create_group/', views.create_group, name='create_group'),
     path('group/<int:pk>/', views.group, name='group'),
 ]
